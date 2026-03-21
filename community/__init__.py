8ea028ba832a0a0fd2cf98913ca02fcd39474bba
mzt006 <mztahir006@gmail.com>
2026-03-21 23:47:58 +0500

    Updated community/__init__.py

--- community/__init__.py
+++ community/__init__.py
@@ -1,4 +1,16 @@
+"""
+community/__init__.py
+Exports sanitizer, consent, client, and history tracker.
+"""
+
 from community.sanitizer import Sanitizer
 from community.consent import ConsentManager
 from community.client import ContributionClient
 from community.history import HistoryTracker
+
+__all__ = [
+    "Sanitizer",
+    "ConsentManager",
+    "ContributionClient",
+    "HistoryTracker",
+]
